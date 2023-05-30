9f5477d0c7f0b174482849db36aa7027f617a227
Jude Mai <jmai1@spscc.edu>
2023-05-29 19:35:15 -0700

    Created a class libary for cars and listing. Created an abstract class called Car. Temporarily made variables representing make, model, color, age, and price.

---
+++ CarLibrary/Car.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+namespace CarLibrary
+{
+    abstract class Car
+    {
+        // The Car class should have all the common properties: make, model, color, age, and price.
+        // Temporary
+        string make = "", model = "", color = "";
+        float age = 0f;
+        decimal price = 0M;
+    }
+}

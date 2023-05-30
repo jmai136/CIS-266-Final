157ce9a141f9f3fe0939ac1660bc7337f03584d5
Jude Mai <jmai1@spscc.edu>
2023-05-30 09:38:54 -0700

    For CarLibrary: Added properties to Car.cs Created Listings.cs and User.cs Added functions for getting all cars, filtering, add, delete in Listing.cs Created a generic in Listings. Added encrypted password method in User

--- CarLibrary/Car.cs
+++ CarLibrary/Car.cs
@@ -4,10 +4,47 @@ namespace CarLibrary
 {
     abstract class Car
     {
+        // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/abstract
         // The Car class should have all the common properties: make, model, color, age, and price.
         // Temporary
-        string make = "", model = "", color = "";
-        float age = 0f;
-        decimal price = 0M;
+
+        /*
+         * Use the abstract modifier in a method or property declaration to 
+         * indicate that the method or property does not contain implementation.
+         * Abstract methods have the following features:
+         * An abstract method is implicitly a virtual method.
+         * Abstract method declarations are only permitted in abstract classes.
+         * 
+         * Because an abstract method declaration provides no actual implementation, there is no method body.
+         * Abstract properties behave like abstract methods, except for the differences 
+         * in declaration and invocation syntax.
+         * It is an error to use the abstract modifier on a static property. 
+         * An abstract inherited property can be overridden in a derived class by 
+         * including a property declaration that uses the override modifier.
+         */
+
+        protected abstract int id { get; set; }
+        protected abstract int userId { get; set; }
+        protected abstract string make { get; set; }
+        protected abstract string model {get; set;}
+        protected abstract string color {get; set; }
+        protected abstract float age { get; set;}
+        protected abstract decimal price { get; set;}
+        protected abstract float miles { get; set; }
+
+        /*
+         * In addition, the subclasses should have one or more other-features as properties.
+         * For example: Engine for BMW, Mileage for Toyota, etc. to highlight that
+         * company's best feature(s)
+         * 
+         * Use the features and filter them
+         * 
+         * Have unique properties in subclasses
+         * Have a huge collection of cars, handle to string
+         * base.ToString(+ inserted string)
+         * 
+         */
+
+        protected abstract string[] features { get; set;}
     }
 }
---
+++ CarLibrary/Listing.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLibrary
+{
+    public class Listing
+    {
+        // should have the properties for Car and the DateTime of the creation of the listing
+        DateTime carCreationTime { get; }
+
+        /*
+         * Change the code so you use Interfaces instead.
+         * Create your own custom interfaces.
+         * Use the in-built interfaces that help you compare different listings while filtering.
+         * (Hint: IComparable)
+         */
+
+        // Select statements here or grab the query created in the designer
+        // Should have all three methods somewhere: creating query through designer, execute scalar, and stored procedures
+        public static void GetAllCars()
+        {
+
+        }
+
+        // Insert in the parameters here
+
+        /*
+         * When a filter option is selected, the application should show subsections with
+         * titles in ascending order.
+         * The titles can include the different makes in ascending order if make is
+         * selected. Then display appropriate car listings underneath each title
+         * ordered by listings' creation times.
+         * There is no need to filter by model so you can skip that.
+         * If color is selected, figure out how many different colors are in your saved
+         * car listings data and create that many subsections with titles as those
+         * colors in ascending order. Then display the cars under the appropriate
+         * color ordered by listings' creation times.
+         * 
+         * The age and price should have ranges. For example: price under $5000,
+         * between $5000 to $9,999, and $10,000+ as the titles for the subsections.
+         * Then display the appropriate car listings in those subsections ordered by
+         * listings' creation times. 
+         */
+        public static void GetCarFilteredBy()
+        {
+
+        }
+
+        public static void UploadCarListing()
+        {
+
+        }
+
+        public static void DeleteCarListing()
+        {
+
+        }
+
+        // Comment section where a potential buyer can ask questions to the seller.
+        // Save in text file?
+        public static void GetComment()
+        {
+
+        }
+    }
+
+    //  Use Generics <T> for the different cars' classes.
+    class CarListed<T>
+    {
+        public T? CarSubclass { get; set; }
+    }
+}
